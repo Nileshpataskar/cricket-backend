b8262c930e1a33f3954ac7727fa5e4e63d094257
Nilesh Pataskar <nileshpataskars@gmail.com>
2025-01-01 06:59:01 +0530

    fixxing error

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-const dotenv = require("dotenv");
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const dotenv = require("dotenv");
 const http = require("http");
 const socketIO = require("socket.io");
-const { Match } = require("./models/match");
 
 dotenv.config();
 
@@ -21,6 +20,28 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("MongoDB connection error:", err));
+
+// Match Schema
+const ballSchema = new mongoose.Schema({
+  runs: { type: Number, default: 0 },
+  isOut: { type: Boolean, default: false },
+  ballNumber: { type: Number, required: true },
+});
+
+const overSchema = new mongoose.Schema({
+  balls: [ballSchema],
+  overNumber: { type: Number, required: true },
+});
+
+const matchSchema = new mongoose.Schema({
+  currentOver: overSchema,
+  overHistory: [overSchema],
+  totalRuns: { type: Number, default: 0 },
+  totalWickets: { type: Number, default: 0 },
+  maxOvers: { type: Number, default: 20 },
+});
+
+const Match = mongoose.model("Match", matchSchema);
 
 // APIs
 
